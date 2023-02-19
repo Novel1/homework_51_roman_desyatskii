112f3db16f9f9c53f0fb535cc45b999c67d32786
Roman Desyatskii <roman.desyatskii30@mail.ru>
2023-02-19 16:27:05 +0600

    Хранение данных.

---
+++ webapp/db.py
@@ -0,0 +1,9 @@
+class DataBase:
+    context = {
+        'media': 'media/cat_2.jpg',
+        'name': '',
+        'year': 1,
+        'happiness': 10,
+        'eat': 40,
+        'status': '',
+    }
